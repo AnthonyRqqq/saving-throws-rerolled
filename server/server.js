1faf808d4140a97596bad8ace141e47ef82928ce
Anthony Rosati <anthony_rosati@yahoo.com>
2026-01-14 18:38:38 -0600

    fix version issues with express middleware, import auth handlers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("@apollo/server");
-const { expressMiddleware } = require("@apollo/server/express4");
+const { expressMiddleware } = require("@as-integrations/express5");
 const path = require("path");
 const cors = require("cors");
 
