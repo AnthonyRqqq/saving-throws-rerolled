e17f41db5fa964f9dd57c3a93b2909a4888b85b0
Anthony Rosati <anthony_rosati@yahoo.com>
2026-01-14 18:46:13 -0600

    template for front end configured

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import "primereact/resources/themes/md-light-deeppurple/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
